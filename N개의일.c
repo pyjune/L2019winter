b559c2eed4015bff4531bfcc5ddf4dab3bfae89f
pyjune <pyjune@naver.com>
2019-01-17 16:57:13 +0900

    Update N개의일.c

--- N개의일.c
+++ N개의일.c
@@ -6,7 +6,7 @@ int t[10][10];
 int minV;
 
 void find(int n, int k);
-
+void find2(int n, int k, int s);
 int main(void) 
 {
 	int T;
@@ -25,10 +25,37 @@ int main(void)
 			}
 		}
 		find(0, N);
+		//find2(0, N, 0);
 		printf("#%d %d\n", tc, minV);
 	}
 	
 	return 0;
+}
+
+void find2(int n, int k, int s)
+{
+	if(n == k)
+	{
+		if(minV > s)
+			minV = s;
+	}
+	else if(s >= minV)
+	{
+		return;
+	}
+	else
+	{
+		for(int i = 0; i < k; i++)
+		{
+			if(used[i] == 0)
+			{
+				used[i] = 1;
+				//p[n] = i;
+				find2(n + 1, k, s+t[i][n]); // s+t[p[n]][n]
+				used[i] = 0;
+			}
+		}
+	}
 }
 
 void find(int n, int k)
